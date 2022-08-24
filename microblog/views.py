87fe2b0ae0279395389f9f542528ec4c475221b4
georgegozal <georgeg9@protonmail.com>
2022-08-24 09:05:18 +0400

    added microblog/views

--- microblog/views.py
+++ microblog/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
 from microblog import app
+from microblog.posts.forms import SearchForm
 
 # Invalid URL
 @app.errorhandler(404)
@@ -12,5 +14,13 @@ def page_not_found(e):
     return render_template('500.html'), 500
 
 @app.route('/')
-def home():
-    return "hello world"
+def index():
+    return render_template(
+        'home.html'
+    )
+
+
+@app.context_processor
+def base():
+    form = SearchForm()
+    return dict(form=form)
