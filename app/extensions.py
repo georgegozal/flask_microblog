d361e8deedbee236dbda092ef33b37bd773e1e1c
georgegozal <georgeg9@protonmail.com>
2022-12-06 13:31:22 +0400

    trying flask-script, and flask-bootstrap

--- app/extensions.py
+++ app/extensions.py
@@ -3,10 +3,15 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_ckeditor import CKEditor
 from flask_mail import Mail
+# from flask_bootstrap import Bootstrap
+# from flask_script import Manager
+
 
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
+# bootstrap = Bootstrap()
+# manager = Manager()
 # Add CKEditor
 ckeditor = CKEditor()
 login_manager = LoginManager()
